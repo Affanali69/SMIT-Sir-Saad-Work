11f765c765cd3a4bac40f7da711fbe1d2a33e4cb
Affan Ali <affannawab087@gmail.com>
2025-03-05 10:11:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+function calculate() {
+    var a = +prompt("Enter the first number:");
+    var b = +prompt("Enter the second number:");
+    var operator = prompt("Enter the operator (+, -, *, /):");
+    
+    var result;
+    if (operator === '+') {
+        result = a + b;
+    } else if (operator === '-') {
+        result = a - b;
+    } else if (operator === '*') {
+        result = a * b;
+    } else if (operator === '/') {
+        if (b === 0) {
+            alert("Error: Division by zero is not allowed.");
+            return;
+        }
+        result = a / b;
+    } else {
+        alert("Error: Invalid operator. Please enter one of the following: +, -, *, /");
+        return;
+    } 
+    
+    alert("Result: " + result);
+
+}
