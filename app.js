03c4bcd3ff42b1e8c282a3a718c478a540975e02
Affan Ali <affannawab087@gmail.com>
2025-03-10 10:25:17 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,53 @@
-function calculate() {
-    var a = +prompt("Enter the first number:");
-    var b = +prompt("Enter the second number:");
-    var operator = prompt("Enter the operator (+, -, *, /):");
-    
-    var result;
-    if (operator === '+') {
-        result = a + b;
-    } else if (operator === '-') {
-        result = a - b;
-    } else if (operator === '*') {
-        result = a * b;
-    } else if (operator === '/') {
-        if (b === 0) {
-            alert("Error: Division by zero is not allowed.");
-            return;
-        }
-        result = a / b;
-    } else {
-        alert("Error: Invalid operator. Please enter one of the following: +, -, *, /");
-        return;
-    } 
-    
-    alert("Result: " + result);
+// function date() {
+//     var time = new Date();
+//     document.write(time);
+// }
+// date();
+//  function name() {
+//     var user = prompt("Enter your first name :");
+//     var last = prompt("Enter your last Name : ");
+//     var fullname = user + " " + last ;
+//     document.write(fullname);
+//  }
+//  name();
 
-}
+// function sum() {
+//     var num1 = +prompt("enter your first number : ");
+//     var num2 = +prompt("enter your last number : ");
+//     var sum = num1 + num2;
+//     document.write(sum);
+// }
+// sum();
 
-function foo() {
-    var getInp = document.getElementById("inp");
-    console.log(getInp.value);
-}
+// function calculator() {
+//     var num1 = +prompt("Enter your first number :");
+//     var num2 = +prompt("Enter your last number : ");
+//     var operator = prompt("Enter Your operation ('+', '-', '*', '/')");
+//     var result;
+
+//     if (operator === '+') {
+//         result = num1 + num2;
+//     } else if (operator === '-'){
+//         result = num1 - num2;
+//     } else if (operator === '*') {
+//         result = num1 * num2;
+//     } else if (operator === '/'){
+//         if (num1 === '0'){
+//             alert("Division By zero is not allowed")
+//         } else {
+//             result = num1 / num2;
+//         }
+//     }
+//     else {
+//         alert("invaild number");
+//     }
+//     document.write(result);
+// }
+// calculator();
+
+// function sqr() {
+//     var number = +prompt ("Enter your number : ");
+//     var final = number * number ;
+//     document.write(final);
+// }
+// sqr();
