2550e360a359ce360a419c4ed5f6f389de070acf
Affan Ali <affannawab087@gmail.com>
2025-03-10 10:26:33 +0500

    Add files via upload

---
+++ work.js
@@ -0,0 +1,53 @@
+// function date() {
+//     var time = new Date();
+//     document.write(time);
+// }
+// date();
+//  function name() {
+//     var user = prompt("Enter your first name :");
+//     var last = prompt("Enter your last Name : ");
+//     var fullname = user + " " + last ;
+//     document.write(fullname);
+//  }
+//  name();
+
+// function sum() {
+//     var num1 = +prompt("enter your first number : ");
+//     var num2 = +prompt("enter your last number : ");
+//     var sum = num1 + num2;
+//     document.write(sum);
+// }
+// sum();
+
+// function calculator() {
+//     var num1 = +prompt("Enter your first number :");
+//     var num2 = +prompt("Enter your last number : ");
+//     var operator = prompt("Enter Your operation ('+', '-', '*', '/')");
+//     var result;
+
+//     if (operator === '+') {
+//         result = num1 + num2;
+//     } else if (operator === '-'){
+//         result = num1 - num2;
+//     } else if (operator === '*') {
+//         result = num1 * num2;
+//     } else if (operator === '/'){
+//         if (num1 === '0'){
+//             alert("Division By zero is not allowed")
+//         } else {
+//             result = num1 / num2;
+//         }
+//     }
+//     else {
+//         alert("invaild number");
+//     }
+//     document.write(result);
+// }
+// calculator();
+
+// function sqr() {
+//     var number = +prompt ("Enter your number : ");
+//     var final = number * number ;
+//     document.write(final);
+// }
+// sqr();
