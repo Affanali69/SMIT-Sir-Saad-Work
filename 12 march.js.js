3b037a9aed74a53b49e9afb74a5644a519e6cf96
Affan Ali <affannawab087@gmail.com>
2025-03-12 10:26:38 +0500

    Add files via upload

---
+++ 12 march.js.js
@@ -0,0 +1,5 @@
+var h1 = document.querySelector('#head')
+h1.innerHTML = "changed through js"
+
+var p = document.querySelector('#para')
+p.innerHTML = "humne java script ka use kartey huei html ka element changed kar liya"
