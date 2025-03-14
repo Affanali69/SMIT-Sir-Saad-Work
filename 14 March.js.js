f3c63ed0f319d6e981d02505143a0678bb41f2aa
Affan Ali <affannawab087@gmail.com>
2025-03-14 10:24:21 +0500

    Add files via upload

---
+++ 14 March.js.js
@@ -0,0 +1,9 @@
+var user = prompt ("Enter your Email: ").toLowerCase();
+
+switch (user){
+    case "affannawab087@gmail.com" : 
+    location.href = "./work.html";
+    break;
+    default :
+    document.write("Plz Enter a Email!")
+}
